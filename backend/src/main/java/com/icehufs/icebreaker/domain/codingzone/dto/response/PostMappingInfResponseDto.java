7aa324b5e5aba2a29341758b683e37cf8aa0b436
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-29 00:19:17 +0900

    [BE-#230] feature : 코딩존 매핑 기능을 위한 ResponseDto 정의

---
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/dto/response/PostMappingInfResponseDto.java
@@ -0,0 +1,18 @@
+package com.icehufs.icebreaker.domain.codingzone.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostMappingInfResponseDto {
+
+  private String code;
+  private String message;
+
+  
+}
