c3a8fa669ac8dfd18789e4c2f797638c0aa5bc36
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-29 22:27:44 +0900

    refactor : controller 응답 반환 전용 Dto 네이밍 변경

---
+++ backend/src/main/java/com/icehufs/icebreaker/util/SubjectResponseDto.java
@@ -0,0 +1,12 @@
+package com.icehufs.icebreaker.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SubjectDto {
+
+    private Integer subjectId;
+    private String subjectName;
+}
