510002bf09edc41392016bff8c83bde07eaf71fb
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-29 16:36:04 +0900

    refactor : mappinginf 테이블명을 subject로 수정으로 인한 controller 리펙토링

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/controller/MappingCodingZoneController.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/controller/MappingCodingZoneController.java
@@ -1,49 +1,35 @@
 package com.icehufs.icebreaker.domain.codingzone.controller;
 
 import java.util.List;
-
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.icehufs.icebreaker.domain.codingzone.domain.entity.MappingInfo;
-import com.icehufs.icebreaker.domain.codingzone.dto.request.PostMappingInfoRequestDto;
-import com.icehufs.icebreaker.domain.codingzone.dto.response.GetMappingInfoResponseDto;
-import com.icehufs.icebreaker.domain.codingzone.dto.response.PostMappingInfoResponseDto;
-import com.icehufs.icebreaker.domain.codingzone.service.GroupInfoService;
+import com.icehufs.icebreaker.domain.codingzone.dto.request.PostSubjectMappingRequestDto;
+import com.icehufs.icebreaker.domain.codingzone.dto.response.PostSubjectMappingResponseDto;
+import com.icehufs.icebreaker.domain.codingzone.service.SubjectService;
 import com.icehufs.icebreaker.util.ResponseDto;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/admin/subject-aliases") // 코딩존 메핑 관련 controller 
 @RequiredArgsConstructor
 public class MappingCodingZoneController {
 
-    private final GroupInfoService groupInfoService;
+    private final SubjectService subjectService;
 
   // 코딩존 매핑 등록 controller
     @PostMapping("")
-    public ResponseEntity<ResponseDto<PostMappingInfoResponseDto>> postMappingInf(
+    public ResponseEntity<ResponseDto<PostSubjectMappingResponseDto>> postSubjectMapping(
         @AuthenticationPrincipal String email,  //확인하고자하는 유저의 토큰 유효성 확인 후 유저의 메일 반환
-        @Valid @RequestBody List<PostMappingInfoRequestDto> requestDto // 매핑이 여러개 들어올 수 있으니 List<>로
+        @Valid @RequestBody List<PostSubjectMappingRequestDto> requestDto // 매핑이 여러개 들어올 수 있으니 List<>로
     ){
-        return ResponseEntity.ok(ResponseDto.success(groupInfoService.postMappingCodingZoneClass(requestDto, email)));
+        return ResponseEntity.ok(ResponseDto.success(subjectService.postMappingCodingZoneClass(requestDto, email)));
     }
 
-    /*  
-    @GetMapping("")
-    public ResponseEntity<ResponseDto<GetMappingInfoResponseDto<List<MappingInfo>>>> getMappingInf(
-        @AuthenticationPrincipal String email // 조회는 따로 RequestBody 필요 없음
-    ){
-        return ResponseEntity.ok(ResponseDto.success(groupInfoService.getMappingCodingZoneClass(email)));
-    }
-    */
+
 
 }
