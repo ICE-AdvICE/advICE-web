83d5e9b73ace5c13ef022f7050f791c075027c4c
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-29 16:38:34 +0900

    refactor : mappinginf 테이블명을 subject로 수정으로 인한 Repository 리펙토링

---
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/SubjectRepository.java
@@ -0,0 +1,16 @@
+package com.icehufs.icebreaker.domain.codingzone.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.icehufs.icebreaker.domain.codingzone.domain.entity.Subject;
+
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+
+    boolean existsBySubjectId(Integer subjectId);
+    boolean existsBySubjectName(String subjectName);
+    boolean existsBySubjectIdIsNotNull();
+
+    // subjectId 오름차순 정렬 조회
+    List<Subject> findAllByOrderBySubjectIdAsc();
+}
