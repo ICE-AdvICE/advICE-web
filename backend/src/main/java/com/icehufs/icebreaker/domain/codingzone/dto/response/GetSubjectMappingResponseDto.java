5f917a97049c81f0634139bb685bf6d51737fab1
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-29 22:44:27 +0900

    fix : 불필요한 인덴테이션 제거

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/dto/response/GetSubjectMappingResponseDto.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/dto/response/GetSubjectMappingResponseDto.java
@@ -1,17 +1,15 @@
 package com.icehufs.icebreaker.domain.codingzone.dto.response;
 
 import java.util.List;
-
-import com.icehufs.icebreaker.util.SubjectDto;
+import com.icehufs.icebreaker.util.SubjectResponseDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 
 @Getter
 @AllArgsConstructor
 public class GetSubjectMappingResponseDto {
 
-  private List<SubjectDto> subjectList;
+  private List<SubjectResponseDto> subjectList;
   
 }
