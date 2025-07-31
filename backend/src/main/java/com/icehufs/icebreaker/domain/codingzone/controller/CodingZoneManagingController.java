d6e9eee28bed765734b55d4aa9cb0fdfb6a38b3c
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-31 14:09:45 +0900

    feature : 코딩존 수업 등록 controller 구현

---
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/controller/CodingZoneManagingController.java
@@ -0,0 +1,37 @@
+package com.icehufs.icebreaker.domain.codingzone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.icehufs.icebreaker.domain.codingzone.dto.request.CodingZoneClassAssignRequestDto;
+import com.icehufs.icebreaker.domain.codingzone.service.CodingZoneManagingService;
+import com.icehufs.icebreaker.util.ResponseDto;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/admin/codingzones")
+@RequiredArgsConstructor
+public class CodingZoneManagingController {
+
+  private final CodingZoneManagingService codingZoneManagingService;
+
+  @PostMapping("")
+  public ResponseEntity<ResponseDto<Void>> postCodingZoneClass(
+    @AuthenticationPrincipal String email,
+    @Valid @RequestBody List<CodingZoneClassAssignRequestDto> requestBody) {
+
+      codingZoneManagingService.postMappingCodingZoneClass(requestBody, email);
+      return ResponseEntity.ok(ResponseDto.success(null));
+  }
+}
+
