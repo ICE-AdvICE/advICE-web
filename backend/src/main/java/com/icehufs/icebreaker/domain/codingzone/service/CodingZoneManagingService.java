1d9200d4a3d91a0700fb70568eeaaf61351e0951
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-31 18:43:24 +0900

    fix : 불필요한 예외처리 삭제 및 DB에 이미 존재하는 수업 확인 후 예외처리 추가

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/CodingZoneManagingService.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/CodingZoneManagingService.java
@@ -14,6 +14,7 @@ import com.icehufs.icebreaker.domain.codingzone.repository.CodingZoneClassReposi
 import com.icehufs.icebreaker.domain.codingzone.repository.GroupInfRepository;
 import com.icehufs.icebreaker.domain.codingzone.repository.SubjectRepository;
 import com.icehufs.icebreaker.exception.BusinessException;
+import com.icehufs.icebreaker.util.CodingZoneClassIdentityDto;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -32,14 +33,33 @@ public class CodingZoneManagingService {
     for(CodingZoneClassAssignRequestDto requestDto : dto) {
       if(!subjectRepository.existsBySubjectId(requestDto.getSubjectId())) 
         throw new BusinessException("400", "등록하려고 하는 교과목 중 이름 매핑 작업이 이루어지지 않은 것이 있습니다, 매핑을 먼저 해주세요!", HttpStatus.BAD_REQUEST);
-    }
-      
-    Set<String> dateSet = new HashSet<>(); // 들어온 Dto 객체들 중 중복 검사
 
+        // 등록 하려는 필드 값들이 DB에서 모두 같은 (currentNumber제외)경우가 있을 때 예외처리
+        CodingZoneClassIdentityDto identity = new CodingZoneClassIdentityDto(requestDto);
+        boolean isDuplicate = codingZoneClassRepository
+            .existsByIdentity(
+            identity.getAssistantName(),
+            identity.getClassDate(),
+            identity.getClassTime(),
+            identity.getClassName(),
+            identity.getMaximumNumber(),
+            identity.getWeekDay(),
+            identity.getSubjectId(),
+            identity.getGroupId()
+        );
+
+      if (isDuplicate) {
+          throw new BusinessException("409", "해당 수업은 이미 등록되어 있습니다.", HttpStatus.CONFLICT);
+      }
+    }
+  
+    Set<CodingZoneClassAssignRequestDto> dtoSet = new HashSet<CodingZoneClassAssignRequestDto>(dto);
+    // 들어온 Dto 객체들 중 중복 검사
     //수업 등록 진행 시 모든 필드가 동일한 dto를 set과 List를 이용해서 비교
-    if (dateSet.size() < dto.size())  //set의 사이즈와 List의 사이즈가 다를 경우에 중복이 있다는 것이기 때문에 예외처리
+    if (dtoSet.size() < dto.size())  //set의 사이즈와 List의 사이즈가 다를 경우에 중복이 있다는 것이기 때문에 예외처리
     throw new BusinessException("400", "전체 내용 중복 코딩존 등록이 있습니다.", HttpStatus.BAD_REQUEST);
         
+
     // 수업 + 조등록 가능!
     for (CodingZoneClassAssignRequestDto requestDto : dto) {
       CodingZoneClass codingZoneClassEntity = new CodingZoneClass(requestDto);
