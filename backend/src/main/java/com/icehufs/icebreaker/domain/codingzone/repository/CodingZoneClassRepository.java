ba0d31fac3709fb17eef234ef03b2b49a2b28583
Dayoung <pdayoung0402@hufs.ac.kr>
2025-07-31 18:41:04 +0900

    refactor : CodingZoneClassIdentityDto 사용으로 인한 reRepository 수정

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneClassRepository.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneClassRepository.java
@@ -2,9 +2,13 @@ package com.icehufs.icebreaker.domain.codingzone.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.icehufs.icebreaker.domain.codingzone.domain.entity.CodingZoneClass;
+import com.icehufs.icebreaker.util.CodingZoneClassIdentityDto;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 
 
@@ -14,5 +18,28 @@ public interface CodingZoneClassRepository extends JpaRepository<CodingZoneClass
     CodingZoneClass findByClassNum(Integer classNum);  
     List<CodingZoneClass> findBySubjectId(int subjectId);
     List<CodingZoneClass> findBySubjectIdAndClassDateBetween(int subjectId, String startDate, String endDate);
+
+    @Query("""
+        SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END
+        FROM codingzoneclass c
+        WHERE c.assistantName = :assistantName
+        AND c.classDate = :classDate
+        AND c.classTime = :classTime
+        AND c.className = :className
+        AND c.maximumNumber = :maximumNumber
+        AND c.weekDay = :weekDay
+        AND c.subjectId = :subjectId
+        AND c.groupId = :groupId
+    """)
+    boolean existsByIdentity(
+    @Param("assistantName") String assistantName,
+    @Param("classDate") String classDate,
+    @Param("classTime") String classTime,
+    @Param("className") String className,
+    @Param("maximumNumber") int maximumNumber,
+    @Param("weekDay") String weekDay,
+    @Param("subjectId") int subjectId,
+    @Param("groupId") String groupId
+);
 
 }
