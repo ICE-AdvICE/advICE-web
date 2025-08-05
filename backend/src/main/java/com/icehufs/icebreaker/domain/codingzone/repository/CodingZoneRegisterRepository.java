78e84852f7776e420a8f3ee35aa2e216c89c7506
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-05 16:51:25 +0900

    create : 주석 추가

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneRegisterRepository.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneRegisterRepository.java
@@ -16,5 +16,9 @@ public interface CodingZoneRegisterRepository extends JpaRepository<CodingZoneRe
     List<CodingZoneRegister> findByClassNum(int classNum);
     List<CodingZoneRegister> findByUserEmail(String userEmail);
     List<CodingZoneRegister> findAllByOrderByUserStudentNumAsc();
-    List<CodingZoneRegister> findByClassNumOrderByUserStudentNumAsc(Integer classNum);
+    
+    // codingzoneclass와 codingzoneregister 쿼리 연결
+    // codingzoneclass로 부터 grade -> classNum 필드값 추출
+    // classNum으로 부터 codingzoneregister 객체 추출
+    List<CodingZoneRegister> findByClassNumOrderByUserStudentNumAsc(Integer classNum); 
 }
