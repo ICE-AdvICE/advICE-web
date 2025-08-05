7a3048c281e5bc3fe49e654db1958f92ad70fb0c
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-05 16:48:00 +0900

    create : codingzoneclass와 codingzoneregister 쿼리 연결로 인한 레포 수정

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneRegisterRepository.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneRegisterRepository.java
@@ -3,17 +3,18 @@ package com.icehufs.icebreaker.domain.codingzone.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.icehufs.icebreaker.domain.codingzone.domain.entity.CodingZoneRegister;
+import com.icehufs.icebreaker.domain.codingzone.domain.entity.Subject;
+
 import java.util.List;
 
 @Repository
 public interface CodingZoneRegisterRepository extends JpaRepository<CodingZoneRegister, Integer> {
 
     CodingZoneRegister findByClassNumAndUserEmail(Integer classNum, String userEmail);
-
     CodingZoneRegister findByRegistrationId(Integer registrationId);
-    List<CodingZoneRegister> findBySubjectId(int subjectId);
+    List<CodingZoneRegister> findByClassNum(Integer classNum);
+    List<CodingZoneRegister> findByClassNum(int classNum);
     List<CodingZoneRegister> findByUserEmail(String userEmail);
-
     List<CodingZoneRegister> findAllByOrderByUserStudentNumAsc();
-    List<CodingZoneRegister> findBySubjectIdOrderByUserStudentNumAsc(int subjectId);
+    List<CodingZoneRegister> findByClassNumOrderByUserStudentNumAsc(Integer classNum);
 }
