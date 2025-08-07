6b8ef39609a2b44f469b290ccfbb358f5837e032
dkanrjskgkwkhufs <innew@hufs.ac.kr>
2025-08-08 00:12:19 +0900

    feat : service 로직에 getCodingZoneClassNamesByDate 메서드 추가

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/implement/CodingZoneServiceImplement.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/implement/CodingZoneServiceImplement.java
@@ -619,6 +619,19 @@ public class CodingZoneServiceImplement implements CodingZoneService {
     }
 
     @Override
+    public CodingZoneClassNamesResponseDto getCodingZoneClassNamesByDate(String date) {
+        DayOfWeek day = LocalDate.parse(date).getDayOfWeek();
+        if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) throw new BusinessException("INVAIlD_DATE_WEEKEND", "입력한 날짜가 주말임", HttpStatus.BAD_REQUEST);
+        List<CodingZoneClass> codingZoneClasses = codingZoneClassRepository.findAllByClassDate(date);
+        if (codingZoneClasses.isEmpty()) throw new BusinessException("NO_CODINGZONE_DATE", "입력한 평일에 코딩존이 없음", HttpStatus.BAD_REQUEST);
+        List<String> classNames = codingZoneClasses.stream()
+                .map(CodingZoneClass::getClassName)
+                .distinct()
+                .toList();
+        return new CodingZoneClassNamesResponseDto(classNames);
+    }
+
+    @Override
     public AssistantNamesResponseDto getAssistantNamesBySubjectId(Long subjectId) {
         List<Authority> authorityList = switch (subjectId.intValue()) {
             case 1 -> authorityRepository.findByRoleAdminC1("ROLE_ADMINC1");
