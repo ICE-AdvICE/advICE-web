b6f13e68e6f26c74868a3b63f4af03c09e01698c
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-08 04:21:44 +0900

    refactor: 코딩존 수업 수정 비즈니스 로직 추가 및 수업 등록에서의 예외처리 방식을 커스텀 객체로 생성하도록 수정

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/CodingZoneClassService.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/CodingZoneClassService.java
@@ -3,6 +3,11 @@ package com.icehufs.icebreaker.domain.codingzone.service;
 import java.util.List;
 
 import com.icehufs.icebreaker.domain.codingzone.domain.entity.Subject;
+import com.icehufs.icebreaker.domain.codingzone.dto.request.CodingZoneClassUpdateRequestDto;
+import com.icehufs.icebreaker.domain.codingzone.exception.CodingZoneAlreadyExistsException;
+import com.icehufs.icebreaker.domain.codingzone.exception.DuplicateClassException;
+import com.icehufs.icebreaker.domain.codingzone.exception.NoExistedGroupException;
+import com.icehufs.icebreaker.domain.codingzone.exception.UnmappedSubjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -27,14 +32,13 @@ public class CodingZoneClassService {
     private final GroupInfRepository groupInfRepository;
     private final SubjectRepository subjectRepository;
 
+    // 코딩존 등록
     @Transactional
     public void postClassAndGroup(List<CodingZoneClassAssignRequestDto> dto, String email) {
 
         for (CodingZoneClassAssignRequestDto assignedClass : dto) {
             // 매핑 작업을 하지 않은 교과목에 해당하는 코딩존 수업을 등록하려고 할때
-            if (!subjectRepository.existsById(assignedClass.getSubjectId()))
-                throw new BusinessException(ResponseCode.NOT_MAPPED_CLASS, ResponseMessage.NOT_MAPPED_CLASS,
-                        HttpStatus.CONFLICT);
+            if (!subjectRepository.existsById(assignedClass.getSubjectId())) throw new UnmappedSubjectException();
 
             // 등록 하려는 필드 값들이 DB에서 모두 같은 (currentNumber제외)경우가 있을 때 예외처리
             boolean isDuplicate = codingZoneClassRepository
@@ -48,11 +52,8 @@ public class CodingZoneClassService {
                             assignedClass.getSubjectId()
                             );
 
-            if (isDuplicate) { // 이미 시전에 동일한 수업을 등록한 경우
-                throw new BusinessException(ResponseCode.ALREADY_EXISTED_CLASS, ResponseMessage.ALREADY_EXISTED_CLASS,
-                        HttpStatus.CONFLICT);
-            }
-
+            // 이미 시전에 동일한 수업을 등록한 경우
+            if (isDuplicate) throw new DuplicateClassException();
         }
 
         // 수업 + 조 등록
@@ -65,6 +66,54 @@ public class CodingZoneClassService {
             GroupInf groupInf = new GroupInf(groupDto);
             groupInfRepository.save(groupInf); // 조 등록
         }
+    }
+
+    // 코딩존 정보 수정
+    @Transactional
+    public void patchClassAndGroup(CodingZoneClassUpdateRequestDto dto, Integer classNum) {
+
+        // 수정한 정보가 기존의 정보와 동일할 때 예외처리
+        // 수정하고자 하는 코딩존 수업의 고유번호를 가져와서 DB확인
+        CodingZoneClass existingClass = codingZoneClassRepository.findByClassNum(classNum);
+        if(dto.isSameEntity(existingClass)) throw new CodingZoneAlreadyExistsException();
+
+        // DB 저장된 원래 codingzoneclass 수업 삭제
+
+        CodingZoneClass existedClass = codingZoneClassRepository.findByClassNum(classNum);
+        codingZoneClassRepository.removeCodingZoneClassByClassNum(classNum);
+
+        // DB 저장된 원래 groupinf 조 삭제
+        GroupInf group = groupInfRepository.findByAssistantNameAndClassTimeAndClassName(existedClass.getAssistantName(), existedClass.getClassTime(),existedClass.getClassName())
+                .orElseThrow(() -> new NoExistedGroupException());
+        groupInfRepository.delete(group);
+
+        // 수정된 정보에 아직 매핑 전 교과목이 포함되었을 경우 예외처리
+        if (!subjectRepository.existsById(dto.getSubjectId())) throw new UnmappedSubjectException();
+
+        // 수정한 정보가 이미 DB에 저장되어 있는 경우
+        boolean isDuplicate = codingZoneClassRepository
+                .existsByIdentity(
+                        dto.getAssistantName(),
+                        dto.getClassDate(),
+                        dto.getClassTime(),
+                        dto.getClassName(),
+                        dto.getMaximumNumber(),
+                        dto.getWeekDay(),
+                        dto.getSubjectId()
+                );
+
+        // 이미 시전에 동일한 수업을 등록한 경우 예외처리
+        if (isDuplicate) throw new DuplicateClassException();
+
+        // 수정한 정보로 수업 등록
+        Subject subject = subjectRepository.findById(dto.getSubjectId()) .orElseThrow(() -> new UnmappedSubjectException());
+        CodingZoneClass codingZoneClassEntity = new CodingZoneClass(dto, classNum, subject);
+        codingZoneClassRepository.save(codingZoneClassEntity);
+        // 수정한 정보로 조 등록
+        GroupInfUpdateRequestDto groupDto = new GroupInfUpdateRequestDto(dto);
+        GroupInf groupInf = new GroupInf(groupDto);
+        groupInfRepository.save(groupInf); // 조 등록
+
     }
 
 }
