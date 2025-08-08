eb97ab6afa7cb09a0dd8c92103709b21ce636efa
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-08 17:11:28 +0900

    fix: 예외 상황별 주석 추가

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/exception/UnmappedSubjectException.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/exception/UnmappedSubjectException.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 
 public class UnmappedSubjectException extends BusinessException {
 
+    // 수업 등록+수정 시, 아직 매핑 작업을 하기 전일 때
     public UnmappedSubjectException() {
         super(ResponseCode.NOT_MAPPED_CLASS, ResponseMessage.NOT_MAPPED_CLASS, HttpStatus.CONFLICT);
     }
