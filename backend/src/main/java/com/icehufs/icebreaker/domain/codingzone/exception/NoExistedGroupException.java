d816798e30afad0f2253267f2b1aac0e929e05a4
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-08 17:10:45 +0900

    fix: 수정한 정보가 이미 DB에 존재하는 경우일 때, 응답 코드와 메시지 수정

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/exception/NoExistedGroupException.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/exception/NoExistedGroupException.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 
 public class NoExistedGroupException extends BusinessException {
 
+    // 수업 수정 시, 수정하려고 하는 조 정보가 없을 때
     public NoExistedGroupException() {
         super(ResponseCode.NOT_FOUND_GROUP, ResponseMessage.NOT_FOUND_GROUP, HttpStatus.BAD_REQUEST);
     }
