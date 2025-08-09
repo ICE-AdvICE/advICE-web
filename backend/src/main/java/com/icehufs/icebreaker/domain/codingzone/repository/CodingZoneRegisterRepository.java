ef5ab10721339cfd35e8784dd1fe2eb445d804cb
dkanrjskgkwkhufs <innew@hufs.ac.kr>
2025-08-09 18:35:39 +0900

    feat : CodingZoneRegisterRepository 에 classNum 으로 예약 정보를 찾아올 메서드 추가

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneRegisterRepository.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/repository/CodingZoneRegisterRepository.java
@@ -15,4 +15,5 @@ public interface CodingZoneRegisterRepository extends JpaRepository<CodingZoneRe
     List<CodingZoneRegister> findByUserEmail(String userEmail);
     List<CodingZoneRegister> findAllByOrderByUserStudentNumAsc();
     List<CodingZoneRegister> findAllByCodingZoneClassInOrderByUserStudentNumAsc(List<CodingZoneClass> classes);
+    List<CodingZoneRegister> findByCodingZoneClass_ClassNum(Integer classNum);
 }
