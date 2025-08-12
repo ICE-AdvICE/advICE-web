24087734db34381bfb2d44e06d80d4b1eb777554
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-12 14:50:58 +0900

    refactor: 기존 학기 초기화 메서드(deleteAll) 위치 이동(CodingZoneService -> SemesterMaintenanceService) 및 불필요한 인터페이스 삭제

---
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/SemesterMaintenanceService.java
@@ -0,0 +1,34 @@
+package com.icehufs.icebreaker.domain.codingzone.service;
+
+import com.icehufs.icebreaker.domain.auth.repostiory.AuthorityRepository;
+import com.icehufs.icebreaker.domain.codingzone.repository.CodingZoneClassRepository;
+import com.icehufs.icebreaker.domain.codingzone.repository.CodingZoneRegisterRepository;
+import com.icehufs.icebreaker.domain.codingzone.repository.GroupInfRepository;
+import com.icehufs.icebreaker.domain.codingzone.repository.SubjectRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SemesterMaintenanceService {
+
+    private final CodingZoneClassRepository codingZoneClassRepository;
+    private final CodingZoneRegisterRepository codingZoneRegisterRepository;
+    private final GroupInfRepository groupInfRepository;
+    private final AuthorityRepository authorityRepository;
+    private final SubjectRepository subjectRepository;
+
+    @Transactional
+    public void resetSemester() {
+
+        codingZoneRegisterRepository.deleteAll();
+        groupInfRepository.deleteAll();
+        codingZoneClassRepository.deleteAll();
+        authorityRepository.clearAllRoles();
+        subjectRepository.deleteAll();
+
+    }
+}
