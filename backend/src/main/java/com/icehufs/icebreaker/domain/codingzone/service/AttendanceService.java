86e5995c25045e4b9cea97437acb3a78187b1602
dkanrjskgkwkhufs <innew@hufs.ac.kr>
2025-08-15 01:01:47 +0900

    feat : service 로직 추가 및 toggleAttendance 메서드 entity에 내재화

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/AttendanceService.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/service/AttendanceService.java
@@ -1,9 +1,16 @@
 package com.icehufs.icebreaker.domain.codingzone.service;
 
+import com.icehufs.icebreaker.common.ResponseCode;
+import com.icehufs.icebreaker.common.ResponseMessage;
+import com.icehufs.icebreaker.domain.auth.domain.entity.Authority;
+import com.icehufs.icebreaker.domain.auth.repostiory.AuthorityRepository;
 import com.icehufs.icebreaker.domain.codingzone.domain.entity.CodingZoneRegister;
 import com.icehufs.icebreaker.domain.codingzone.dto.response.ReservationStudentDto;
 import com.icehufs.icebreaker.domain.codingzone.repository.CodingZoneRegisterRepository;
+import com.icehufs.icebreaker.exception.BusinessException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +20,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AttendanceService {
     private final CodingZoneRegisterRepository codingZoneRegisterRepository;
+    private final AuthorityRepository authorityRepository;
 
     public List<ReservationStudentDto> getReservationStudentsByClassNum (Integer classNum) {
         List<CodingZoneRegister> reservations = codingZoneRegisterRepository.findByCodingZoneClassClassNum(classNum);
@@ -24,4 +32,27 @@ public class AttendanceService {
                 ))
                 .collect(Collectors.toList());
     }
+
+
+    @Transactional
+    public Integer updateAttendanceStatus(Integer registNum, String email) {
+        CodingZoneRegister codingZoneRegister = codingZoneRegisterRepository.findByRegistrationId(registNum)
+                .orElseThrow(() -> new BusinessException(ResponseCode.REGISTRATION_NOT_FOUND, ResponseMessage.REGISTRATION_NOT_FOUND, HttpStatus.BAD_REQUEST));
+
+        if (!hasAuthorityForClass(email, codingZoneRegister))
+            throw new BusinessException(ResponseCode.AUTHORIZATION_FAIL, ResponseMessage.AUTHORIZATION_FAIL, HttpStatus.FORBIDDEN);
+
+        return Integer.valueOf(codingZoneRegister.toggleAttendance());
+    }
+
+    private boolean hasAuthorityForClass(String email, CodingZoneRegister reg) {
+        Authority authority = authorityRepository.findByEmail(email);
+        if ("ROLE_ADMIN".equals(authority.getRoleAdmin())) return true;
+
+        Integer subjectId = reg.getCodingZoneClass().getSubject().getId();
+        String requiredRole = "ROLE_ADMINC" + subjectId;
+
+        return authority.hasRole(requiredRole);
+    }
+
 }
