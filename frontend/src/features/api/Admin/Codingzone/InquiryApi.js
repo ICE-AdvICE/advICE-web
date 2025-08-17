289e7a1b5226acc1ee9efccbe7e5939bf32058ce
박준서 <pjseo1313@daum.net>
2025-08-18 01:04:38 +0900

    refactor: 조 정보 반환 api 경로 수정

--- frontend/src/features/api/Admin/Codingzone/InquiryApi.js
+++ frontend/src/features/api/Admin/Codingzone/InquiryApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const DOMAIN = process.env.REACT_APP_API_DOMAIN; 
-const GET_CZ_ASSITANT = () => `${DOMAIN}/api/v1/coding-zone/assistants`;
+const GET_CZ_ASSISTANTS = () => `${DOMAIN}/api/v1/coding-zone/assistants`;
 //21. 코딩존 조교 정보 반환 API
 export const getCzAssistantsRequest =  async () => {
     try {
         const { data } = await axios.get(GET_CZ_ASSISTANTS());
-+        return data; // { code, message, data: [...] }
+        return data; // { code, message, data: [...] }
     } catch (error) {
         if (!error.response || !error.response.data) return null;
         return error.response.data;
