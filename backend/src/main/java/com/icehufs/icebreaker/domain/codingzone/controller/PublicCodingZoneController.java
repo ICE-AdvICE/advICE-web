d9894d7da193c2b3c36cf1128cd064451441b3dd
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-19 15:24:07 +0900

    refactor : 공통 응답 포멧 ResponseDto 적용으로 인한 controller 메소드 이동 및 controller 이름 수정

--- backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/controller/PublicCodingZoneController.java
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/controller/PublicCodingZoneController.java
@@ -2,8 +2,11 @@ package com.icehufs.icebreaker.domain.codingzone.controller;
 
 import java.util.List;
 
+import com.icehufs.icebreaker.domain.codingzone.dto.response.GetAttendanceResponseDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/coding-zone") // 코딩존 일반 사용자 API 주소
 @RequiredArgsConstructor
-public class CodingZoneAssistantController {
+public class PublicCodingZoneController {
 
     private final CodingZoneClassService codingzoneService;
 
@@ -24,6 +27,14 @@ public class CodingZoneAssistantController {
     public ResponseEntity<ResponseDto<List<SubjectAssistantsResponseDto>>> getAssistantList() {
         List<SubjectAssistantsResponseDto> response = codingzoneService.getAssistantList();
         return ResponseEntity.ok(ResponseDto.success("코딩존 과목별 조교 정보 조회에 성공했습니다.", response));
+    }
+
+    @GetMapping("/count-of-attend/{subjectId}") // 출석한 횟수 반환 API
+    public ResponseEntity<ResponseDto<GetAttendanceResponseDto>> getCountAttend(
+            @PathVariable Integer subjectId,
+            @AuthenticationPrincipal String email) {
+        GetAttendanceResponseDto response = codingzoneService.getAttend(subjectId, email);
+        return ResponseEntity.ok(ResponseDto.success("특정 학생의 출석 횟수 조회에 성공했습니다.",response));
     }
 
 }
