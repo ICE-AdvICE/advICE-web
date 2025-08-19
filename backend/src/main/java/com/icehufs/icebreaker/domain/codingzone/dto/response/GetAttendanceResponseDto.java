7c671eb91d143b83dff325988a0307b4b89b22fa
Dayoung <pdayoung0402@hufs.ac.kr>
2025-08-19 15:26:53 +0900

    create : 출석 횟수 반환 필요로 인한 ResponseDto 클래스 생성

---
+++ backend/src/main/java/com/icehufs/icebreaker/domain/codingzone/dto/response/GetAttendanceResponseDto.java
@@ -0,0 +1,12 @@
+package com.icehufs.icebreaker.domain.codingzone.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GetAttendanceResponseDto {
+
+    private Integer numOfAttend;
+
+}
